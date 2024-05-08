0afaa4d08a99549165a91da3d20c1e3c01553166
Vinicius Cerqueira <viniciuscerqueira070@gmail.com>
2024-05-08 19:11:10 -0300

    Desafio Abstraindo um celular com POO em C#

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Marca Iphone");
+Iphone iphone = new Iphone("250","Iphone X","123456789",128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Facebook");
+iphone.ReceberLigacao();
+
+System.Console.WriteLine("\n");
+
+System.Console.WriteLine("Marca Nokia");
+Nokia nokia = new Nokia("1","NOKIA 5.4","123456",64);
+nokia.Ligar();
+nokia.InstalarAplicativo("TikTok");
+nokia.ReceberLigacao();
+
